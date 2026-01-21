ea2429d00893c29a70a87aaef85f6ad1f783c6e5
TomaszLenart <t.lenart234@gmail.com>
2026-01-21 23:30:01 +0100

    Adjust methods to local stores

--- src/app/features/cart/store/cart.store.ts
+++ src/app/features/cart/store/cart.store.ts
@@ -1,13 +1,25 @@
-import { signalStore, withComputed, withProps, withState } from '@ngrx/signals';
+import {
+  signalStore,
+  withComputed,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
 import { initialCartSlice } from './cart.slice';
 import { ShopStore } from '../../../store/shop.store';
 import { computed, inject } from '@angular/core';
 import { buildCartVm } from './cart.vm-builder';
+import { checkoutCart, incrementQuantity } from '../../../store/shop.updaters';
 
 export const CartStore = signalStore(
   withState(initialCartSlice),
   withProps((_) => ({
     _shopStore: inject(ShopStore), // taken from higher level store
+  })),
+  withMethods((store) => ({
+    incrementQuantity: store._shopStore.incrementQuantity,
+    decrementQuantity: store._shopStore.decrementQuantity,
+    checkoutCart: store._shopStore.checkoutCart,
   })),
   withComputed((store) => ({
     vm: computed(() => {
--- src/app/features/product-list/store/product-list.store.ts
+++ src/app/features/product-list/store/product-list.store.ts
@@ -1,4 +1,10 @@
-import { signalStore, withComputed, withProps, withState } from '@ngrx/signals';
+import {
+  signalStore,
+  withComputed,
+  withMethods,
+  withProps,
+  withState,
+} from '@ngrx/signals';
 import { initialProductListSlice } from './product-list.slice';
 import { computed, inject } from '@angular/core';
 import { buildProductListVm } from './product-list.vm-builder';
@@ -8,6 +14,10 @@ export const ProductListStore = signalStore(
   withState(initialProductListSlice),
   withProps((_) => ({
     _shopStore: inject(ShopStore), //taken from higher level store
+  })),
+  withMethods((store) => ({
+    addToCart: store._shopStore.addToCart,
+    viewCart: store._shopStore.viewCart,
   })),
   withComputed((store) => ({
     vm: computed(() =>
